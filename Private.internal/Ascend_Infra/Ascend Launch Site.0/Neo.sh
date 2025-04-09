598fe7504d3acfa3fda10dfdd9d314f44d0b455e
Statik dk Smoke <statiksmoktm@gmail.com>
2025-04-09 00:18:53 -0500

    Update Neo.sh

--- Private.internal/Ascend_Infra/Ascend Launch Site.0/Neo.sh
+++ Private.internal/Ascend_Infra/Ascend Launch Site.0/Neo.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/env python3
+#!/bin/bash
 
 chmod +x AS_SK_C4.py
 ./AS_SK_C4.py
+
 echo "[BOOT] $(date)" >> ascend_boot.log
 echo "Launch full matrix." >> task_queue/init.task
 
-python3 "Private.internal/Ascend_Infra/Launch Ascend/loop_engine.py" &
+python3 "Private.internal/Ascend_Infra/Launch_Ascend/loop_engine.py" &
 
+from datetime import datetime
 with open("ascend_logs/boot_timestamps.log", "a") as f:
     f.write(f"Booted at: {datetime.now()}\n")
 
