686baee5ddfef792af065351c64c5a760330f1ee
Statik DK Smoke <ascend.gremlin@gmail.com>
2025-06-28 13:49:41 -0500

    Update generate_banner.js

--- docs/ticker-bot/generate_banner.js
+++ docs/ticker-bot/generate_banner.js
@@ -1,7 +1,7 @@
-const puppeteer = require("puppeteer");
-const fs = require("fs");
-const path = require("path");
-const { execSync } = require("child_process");
+import puppeteer from "puppeteer";
+import fs from "fs";
+import path from "path";
+import { execSync } from "child_process";
 
 const statsPath = path.join(__dirname, "stats.json");
 const outputGif = path.join(__dirname, "output", "ticker.gif");
