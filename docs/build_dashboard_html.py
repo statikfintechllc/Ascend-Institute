ef51ff31ceda10e611cf8fc3ee463b8b69cb217c
Statik DK Smoke <ascend.gremlin@gmail.com>
2025-06-29 19:42:45 -0500

    Update build_dashboard_html.py

--- docs/build_dashboard_html.py
+++ docs/build_dashboard_html.py
@@ -1,6 +1,6 @@
 # docs/build_dashboard_html.py
 
-with open("docs/graph/dashboard_template.html", "r") as f:
+with open("docs/dashboard_template.html", "r") as f:
     template = f.read()
 
 with open("docs/graph/traffic_totals.json", "r") as f:
@@ -8,5 +8,5 @@ with open("docs/graph/traffic_totals.json", "r") as f:
 
 filled = template.replace("{{TRAFFIC_TOTALS}}", totals.strip())
 
-with open("docs/graph/dashboard.html", "w") as f:
+with open("docs/dashboard.html", "w") as f:
     f.write(filled)
